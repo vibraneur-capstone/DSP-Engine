7911880bec9c3c6a6d3865d7ff01002a79e7c3af
Richard Walmsley <r.walmsley97@gmail.com>
2019-09-27 11:13:03 -0230

    Changed calculations to find Kurtosis using the fourth and second moments of data

--- algorithms/Kurtosis.py
+++ algorithms/Kurtosis.py
@@ -2,11 +2,19 @@ import math
 import statistics
 
 def run(data):
+
+    # Define variables for calculation
     kurtosis = 0
-    stdev = statistics.stdev(data)
+    m4 = 0
+    m2 = 0
     mean = statistics.mean(data)
 
+    # Summation calculations for fourth moment and variance
     for d in data:
-        kurtosis += (d - mean)**4/len(data)/(stdev**4) 
+        m4 += ((d - mean) ** 4) / len(data) # Calculate the fourth moment
+        m2 += ((d - mean) ** 2) / len(data) # Calculate the variance (second moment)
+
+    # Calculate kurtosis with the fourth moment divided by the square of the variance
+    kurtosis = m4 / (m2 ** 2)
 
     print(kurtosis)
