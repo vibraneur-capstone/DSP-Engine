a6f92f5dd171cf64459b02b74df2ead6fde00540
Richard Walmsley <r.walmsley97@gmail.com>
2019-09-27 09:33:23 -0230

    Implemented a Kurtosis script!

--- algorithms/Kurtosis.py
+++ algorithms/Kurtosis.py
@@ -1,3 +1,12 @@
+import math
+import statistics
+
 def run(data):
     kurtosis = 0
+    stdev = statistics.stdev(data)
+    mean = statistics.mean(data)
+
+    for d in data:
+        kurtosis += (d - mean)**4/len(data)/(stdev**4) 
+
     print(kurtosis)
